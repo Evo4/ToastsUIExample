c858aea4588d01b030771388782deac674dabdea
Vyacheslav Razumeenko <dnc.slava.razumeenko@gmail.com>
2025-01-30 17:58:37 +0200

    Initial Commit

---
+++ ToastsUIExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToastsUIExample
+//
+//  Created by Vyacheslav Razumeenko on 30.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToastsUIExample/ToastsUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastsUIExampleApp.swift
+//  ToastsUIExample
+//
+//  Created by Vyacheslav Razumeenko on 30.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToastsUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
